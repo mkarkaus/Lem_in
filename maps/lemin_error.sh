a40071c1209d4b24c45f81ab1ec423f9ffec0cf2
sajereij <sajereij@muru.localdomain>
2020-10-30 11:46:42 +0200

    new maps added for testing

--- maps/lemin_error.sh
+++ maps/lemin_error.sh
@@ -37,7 +37,15 @@ echo " "
 echo "*12. Non-existant map:"
 ./lem-in < maps/murmeli.map
 echo " "
-
+echo "?. Link is left open:" #should not print the contents of farm?
+./lem-in < maps/p_open_link.map
+echo " "
+echo "?. Link is left open b:" #should not print the contents of farm?
+./lem-in < maps/p_open_link.map
+echo " "
+echo "?. Link is left open (no line):" #should not print the contents of farm?
+./lem-in < maps/p_open_link_no_line.map
+echo " "
 echo "<<< NEXT SHOULD BE VALID >>>"
 echo " "
 echo "*Valid 1:"
@@ -63,30 +71,47 @@ echo "*Map with comments and extra commands:"
 echo " "
 echo "*Map with comments:"
 ./lem-in < maps/v_with_comments.map
+echo " "
+echo "*Valid: Big map:"
+./lem-in < maps/v_big_map.map
+echo " "
+echo "*Valid: Test map:" #says invalid
+./lem-in < maps/v_test_map.map
+echo " "
+echo "*Valid: Anamy's map with 1 ant:"
+./lem-in < maps/v_anamy1.map
+echo " "
+# echo "*Valid: Anamy's map with 15 ants:"
+# ./lem-in < maps/v_anamy10.map
+# echo " "
+# echo "*Valid: Anamy's map with 120 ants:"
+# ./lem-in < maps/v_anamy1000.map
+# echo " "
+# echo "*Valid: Anamy's that-map:"
+# ./lem-in < maps/v_anamy_that.map
+# echo " "
+# echo "*Valid: Anamy's huge map:"
+# ./lem-in < maps/v_huge_map.map
+# echo " "
 
 echo "<<< PONDERING >>>"
 echo " "
-echo "?. Giving same coors to two separate rooms:" #should not print the contents of farm?
+echo "?. Giving same coors to two separate rooms:" #should not print the contents of farm? FINE
 ./lem-in < maps/p_giving_same_coor.map
 echo " "
-echo "?. Same name, different co-ors:" #should not print the contents of farm?
-./lem-in < maps/p_giving_same_coor.map
+echo "?. Same name, different co-ors:" #should not print the contents of farm? //ERROR
+./lem-in < maps/p_same_room_diff_coors.map
 echo " "
-echo "?. Listing the same room and co-ors twice:" #should not print the contents of farm?
+echo "?. Listing the same room and co-ors twice:" #should not print the contents of farm? FINE
 ./lem-in < maps/p_listing_same_rm_twice.map
 echo " "
 echo "?. Link to itself:" #should not print the contents of farm?
 ./lem-in < maps/p_link_to_itself.map
 echo " "
-echo "?. Link is left open:" #should not print the contents of farm?
-./lem-in < maps/p_open_link.map
-echo " "
-echo "?. Link is left open b:" #should not print the contents of farm?
-./lem-in < maps/p_open_link.map
-echo " "
-echo "?. Link is left open (no line):" #should not print the contents of farm?
-./lem-in < maps/p_open_link_no_line.map
-echo " "
-echo "?. Start is same as end:" #should not print the contents of farm?
+echo "?. Start is same as end:" #should not print the contents of farm? Special case
 ./lem-in < maps/p_start_is_end.map
 echo " "
+
+#menee yli tai ali intistä
+#no rooms
+#no links
